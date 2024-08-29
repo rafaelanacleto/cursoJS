5c9b257752f9b237d4b2ed9132ed6807851359f2
rafael.anacleto <rafael.anacleto@7comm.com.br>
2024-08-29 10:45:04 -0300

    tete

--- ajax/server.js
+++ ajax/server.js
@@ -1,11 +1,33 @@
 const bodyParser = require('body-parser')
 const express = require('express')
+const multer = require("multer") //interpretar formulario
 
 const app = express()
 
 app.use(express.static('.')) //dentro da pasta atual, sirva os arquivos staticos
 app.use(bodyParser.urlencoded({ extended: true})) //
 app.use(bodyParser.json()) //se vier um json dentro do corpo da requisao
+
+const storage = multer.diskStorage({
+    destination: function(req, file, callback) {
+        callback(null, './upload')
+    },
+    filename: function (req, file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({storage}).single('arquivo')
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if(err) {
+            return res.end("Ocorreu um erro.")
+        }
+        res.end("Concluido com Sucesso!")
+    })
+})
+
 
 app.get('/teste', (req, res) => res.send('Ok'))
 app.listen(8080, () => console.log("Executando.."))
